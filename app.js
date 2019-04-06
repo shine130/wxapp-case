40442957b72b9b70ca8fe059f26f7b8f373f214b
shine130 <shine130@vip.qq.com>
2019-04-06 22:39:05 +0800

    完成

---
+++ app.js
@@ -0,0 +1,17 @@
+App({
+  globalData:{
+    
+  },
+  onLaunch(){
+    wx.request({
+      url:'https://resources.ninghao.net/wxapp-case/db.json',
+      success:(response) => {
+        Object.assign(this.globalData,response.data)
+        const currentPages = getCurrentPages()
+        if(currentPages.length !== 0){
+          currentPages[currentPages.length - 1].onLoad()
+        }
+      }
+    })
+  }
+})
